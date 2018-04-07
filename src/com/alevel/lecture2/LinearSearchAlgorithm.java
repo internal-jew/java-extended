79ddf066cdcc4989019dee21b9094f01ecaca379
Vladyslav Lubenskyi <vladyslav.lubenskyi@teamdev.com>
2018-04-07 13:38:31 +0300

    Add task explanations

--- src/com/alevel/lecture2/LinearSearchAlgorithm.java
+++ src/com/alevel/lecture2/LinearSearchAlgorithm.java
@@ -6,6 +6,13 @@ package com.alevel.lecture2;
 class LinearSearchAlgorithm {
 
     static boolean find(int needle, int[] stack) {
+        // Здесь вам необходимо реализовать алгоритм линейного поиска.
+        // Ваша задача найти число needle в массиве чисел stack.
+        // Если ваш алгоритм нашел искомое число, он должен выполнить такой код:
+        //      return true;
+        // похожий, на строку "return false", которую вы видите в конце метода.
+        //
+        // Переменные needle и stack уже объявлены и заполнены значениями.
         return false;
     }
 }
