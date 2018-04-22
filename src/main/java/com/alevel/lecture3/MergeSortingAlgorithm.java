a17ae3b642a5595e85f73d27033d8d69976c248b
s.bogovesov <bogovesov@gmail.com>
2018-04-22 23:41:36 +0300

    Implementation of algorithms for sorting arrays.

--- src/main/java/com/alevel/lecture3/MergeSortingAlgorithm.java
+++ src/main/java/com/alevel/lecture3/MergeSortingAlgorithm.java
@@ -7,7 +7,39 @@ public class MergeSortingAlgorithm implements SortingAlgorithm {
 
     @Override
     public int[] sort(int[] source) {
-        // Здесь необходимо реализовать сортировку слиянием.
-        return new int[0];
+        int[] a = mergeSort(source, 0, source.length);
+        return a;
+    }
+
+    private int[] mergeSort(int[] numbers, int minIndex, int maxIndex) {
+        if (minIndex + 1 < maxIndex) {
+
+            int midIndex = (minIndex + maxIndex) / 2;
+
+            mergeSort(numbers, minIndex, midIndex);
+            mergeSort(numbers, midIndex, maxIndex);
+
+            int sizeOfTempArray = maxIndex - minIndex;
+            int[] tempArray = new int[sizeOfTempArray];
+
+            int i = minIndex;
+            int j = midIndex;
+
+            for (int k = 0; k < sizeOfTempArray; k++) {
+
+                boolean condition1 = (j >= maxIndex);
+                boolean condition2 = false;
+
+                if (!condition1)
+                    condition2 = ((i < midIndex) && (numbers[i] < numbers[j]));
+
+                tempArray[k] = condition1 || condition2 ? numbers[i++] : numbers[j++];
+            }
+
+            for (int k = 0; k < sizeOfTempArray; k++) {
+                numbers[minIndex + k] = tempArray[k];
+            }
+        }
+        return numbers;
     }
 }
--- src/main/java/com/alevel/lecture3/QuickSortingAlgorithm.java
+++ src/main/java/com/alevel/lecture3/QuickSortingAlgorithm.java
@@ -9,6 +9,44 @@ public class QuickSortingAlgorithm implements SortingAlgorithm {
     public int[] sort(int[] source) {
         // Здесь необходимо реализовать алгоритм быстрой сортировки.
         // Это необязательное задание повышенной сложности.
-        return new int[0];
+        int[] arrRes = quickSort(source, 0, source.length - 1);
+        return arrRes;
+    }
+
+    private int[] quickSort(int[] array, int low, int high) {
+
+        if (low >= high)
+            return array;
+
+        int middle = low + (high - low) / 2;
+        int supportingElement = array[middle];
+
+        int i = low, j = high;
+
+        while (i <= j) {
+            while (array[i] < supportingElement) {
+                i++;
+            }
+
+            while (array[j] > supportingElement) {
+                j--;
+            }
+
+            if (i <= j) {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                i++;
+                j--;
+            }
+        }
+
+        if (low < j)
+            quickSort(array, low, j);
+
+        if (high > i)
+            quickSort(array, i, high);
+
+        return array;
     }
 }
