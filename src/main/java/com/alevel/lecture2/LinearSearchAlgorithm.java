905a7d8443e934ecc5a9351354f03d68696cae76
GpNt <ff25tj@gmail.com>
2018-04-24 21:56:58 +0300

    Implementation of search algorithms

--- src/main/java/com/alevel/lecture2/LinearSearchAlgorithm.java
+++ src/main/java/com/alevel/lecture2/LinearSearchAlgorithm.java
@@ -6,13 +6,12 @@ package com.alevel.lecture2;
 class LinearSearchAlgorithm {
 
     static boolean find(int needle, int[] stack) {
-        // Здесь вам необходимо реализовать алгоритм линейного поиска.
-        // Ваша задача найти число needle в массиве чисел stack.
-        // Если ваш алгоритм нашел искомое число, он должен выполнить такой код:
-        //      return true;
-        // похожий, на строку "return false", которую вы видите в конце метода.
-        //
-        // Переменные needle и stack уже объявлены и заполнены значениями.
+
+        for (int aStack : stack) {
+            if (aStack == needle) {
+                return true;
+            }
+        }
         return false;
     }
 }
