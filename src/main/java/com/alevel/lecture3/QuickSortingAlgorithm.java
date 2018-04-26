a0567cdbd9b083ea4fdb34cfc41f5278f33b47d3
Sergey Bogovesov <bogovesov@gmail.com>
2018-04-26 17:01:02 +0300

    Revert "bogovesov homework 3"

--- src/main/java/com/alevel/lecture3/QuickSortingAlgorithm.java
+++ src/main/java/com/alevel/lecture3/QuickSortingAlgorithm.java
@@ -9,44 +9,6 @@ public class QuickSortingAlgorithm implements SortingAlgorithm {
     public int[] sort(int[] source) {
         // Здесь необходимо реализовать алгоритм быстрой сортировки.
         // Это необязательное задание повышенной сложности.
-        int[] resultArray = quickSort(source, 0, source.length - 1);
-        return resultArray;
-    }
-
-    private int[] quickSort(int[] array, int low, int high) {
-
-        if (low >= high)
-            return array;
-
-        int middle = low + (high - low) / 2;
-        int pivot = array[middle];
-
-        int i = low, j = high;
-
-        while (i <= j) {
-            while (array[i] < pivot) {
-                i++;
-            }
-
-            while (array[j] > pivot) {
-                j--;
-            }
-
-            if (i <= j) {
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                i++;
-                j--;
-            }
-        }
-
-        if (low < j)
-            quickSort(array, low, j);
-
-        if (high > i)
-            quickSort(array, i, high);
-
-        return array;
+        return new int[0];
     }
 }
--- src/main/java/com/alevel/lecture3/SelectionSortingAlgorithm.java
+++ src/main/java/com/alevel/lecture3/SelectionSortingAlgorithm.java
@@ -7,20 +7,7 @@ public class SelectionSortingAlgorithm implements SortingAlgorithm {
 
     @Override
     public int[] sort(int[] source) {
-        for (int i = 0; i < source.length - 1; i++) {
-
-            int minIndex = i;
-
-            for (int j = i + 1; j < source.length; j++) {
-                if (source[j] < source[minIndex]) {
-                    minIndex = j;
-                }
-            }
-
-            int temp = source[i];
-            source[i] = source[minIndex];
-            source[minIndex] = temp;
-        }
-        return source;
+        // Здесь необходимо реализовать сортировку выбором.
+        return new int[0];
     }
 }
